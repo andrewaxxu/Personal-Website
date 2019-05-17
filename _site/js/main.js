b3a8bcba25256d87da5a83095a510a21f9f8d0d2
Andrew Xu <andrew.ax.xu@gmail.com>
2019-05-17 11:16:42 -0400

    Temp card commit

--- _site/js/main.js
+++ _site/js/main.js
@@ -98,3 +98,13 @@
 	});
 
 })(jQuery);
+
+
+var card1 = document.querySelector('#card1');
+card1.addEventListener( 'click', function() {
+  card1.classList.toggle('is-flipped');
+});
+var card2 = document.querySelector('#card2');
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
