49a225b00a1026e42125bb8c7db8b01e164e9db1
Andrew Xu <andrew.ax.xu@gmail.com>
2019-05-17 11:30:45 -0400

    Proper Card Working

--- _site/js/main.js
+++ _site/js/main.js
@@ -108,3 +108,27 @@ var card2 = document.querySelector('#card2');
 card2.addEventListener( 'click', function() {
   card2.classList.toggle('is-flipped');
 });
+var card3 = document.querySelector('#card3');
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+var card4 = document.querySelector('#card4');
+card4.addEventListener( 'click', function() {
+  card4.classList.toggle('is-flipped');
+});
+var card5 = document.querySelector('#card5');
+card5.addEventListener( 'click', function() {
+  card5.classList.toggle('is-flipped');
+});
+var card6 = document.querySelector('#card6');
+card6.addEventListener( 'click', function() {
+  card6.classList.toggle('is-flipped');
+});
+var card7 = document.querySelector('#card7');
+card7.addEventListener( 'click', function() {
+  card7.classList.toggle('is-flipped');
+});
+var card8 = document.querySelector('#card8');
+card8.addEventListener( 'click', function() {
+  card8.classList.toggle('is-flipped');
+});
